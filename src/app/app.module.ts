9fcb8b1756325ed7d2ca24d2ded41b64d80846df
Jose Manuel Pérez <josemanuelperez@comovapp.es>
2018-08-13 17:06:12 +0200

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+
+import { APP_ROUTING } from './app.routes';
+
+// Componentes
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+// Servicioes
+
+import { HeroesService } from './services/heroes.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
